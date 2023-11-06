f833f57f1788a620d46abf42add60102becd53ac
chuannarongvat <narongvat.chingpayakmon@student.uts.edu.au>
2023-11-06 14:46:20 +1100

    Add visualization

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -39,9 +39,9 @@ def plot_bar(df, x, y):
     import seaborn as sns
     import matplotlib.pyplot as plt
     
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(20, 6))
     sns.barplot(data=df, x=x, y=y, palette='ch:.25')
-    plt.xticks(rotation=90, ha='right')
+    plt.xticks(rotation=45, ha='right')
     plt.title(f'Counts by {x}')
     
     plt.show()
@@ -68,3 +68,149 @@ def duration_to_minutes(duration_str):
 def duration_to_hours(duration_str):
     total_minutes = duration_to_minutes(duration_str)
     return total_minutes / 60
+
+def calculate_diff_in_minutes(row):
+    timestamps = row.split('||')
+    if len(timestamps) == 2:
+        return abs(int(timestamps[1]) - int(timestamps[0])) / 60
+    else:
+        return None
+    
+def split_column(df, column_name):
+    if column_name in df.columns:
+        df[[column_name + '_1', column_name + '_2']] = df[column_name].str.split('||', expand=True)
+    else:
+        print(f"The column '{column_name}' does not exist in the DataFrame.")
+    
+    return df
+
+def split_column(df, column_name):
+    if column_name in df.columns:
+        df[[column_name + '_1', column_name + '_2']] = df[column_name].str.split('\|\|', n=1, expand=True)
+    else:
+        print(f"The column '{column_name}' does not exist in the DataFrame.")
+    
+    return df
+
+def split_all_columns(df):
+    for col_name in df.columns:
+        if df[col_name].dtype == 'object' and df[col_name].str.contains('\|\|').any():
+            df[[col_name + '_1', col_name + '_2']] = df[col_name].str.split('\|\|', n=1, expand=True)
+            df.drop(col_name, axis=1, inplace=True)
+    return df
+
+def calculate_flight_duration(df, dep_col_1, arr_col_1, dep_col_2, arr_col_2):
+    df[dep_col_1] = df[dep_col_1].astype(int)
+    df[arr_col_1] = df[arr_col_1].astype(int)
+    df[dep_col_2] = df[dep_col_2].astype(int)
+    df[arr_col_2] = df[arr_col_2].astype(int)
+    
+    df['flight_duration_1'] = (df[arr_col_1] - df[dep_col_1]) / 60
+    
+    df['flight_duration_2'] = (df[arr_col_2] - df[dep_col_2]) / 60
+    
+    return df
+
+def plot_fare_histograms_by_destination(df, start_airport, dest_airport_col, fare_col, hue_col=None, bins=20):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    
+    df_filtered = df[df['startingAirport'] == start_airport]
+    
+    g = sns.FacetGrid(df_filtered, col=dest_airport_col, col_wrap=5, sharex=False, sharey=False, hue=hue_col)
+    
+    hist_kwargs = {'bins': bins, 'kde': False}
+    if hue_col:
+        hist_kwargs['hue'] = df_filtered[hue_col]
+    
+    g = g.map(sns.histplot, fare_col, **hist_kwargs)
+    
+    g.fig.suptitle(f'Histograms of Total Fare from {start_airport} Airport', y=1.02)
+    g.set_axis_labels('Total Fare ($)', 'Frequency')
+    
+    for ax in g.axes.flat:
+        for label in ax.get_xticklabels():
+            label.set_rotation(45)
+    
+    if hue_col:
+        g.add_legend()
+        
+    g.tight_layout()
+    plt.show()
+    
+def plot_fare_trends_by_date_and_destination(df, start_airport, dest_airport_col, date_col, fare_col, hue_col, bins=20):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
+    df[date_col] = pd.to_datetime(df[date_col])
+
+    df_filtered = df[df['startingAirport'] == start_airport]
+    
+    g = sns.FacetGrid(df_filtered, col=dest_airport_col, col_wrap=5, sharex=False, sharey=False, hue=hue_col, legend_out=True)
+    
+    g = g.map(sns.lineplot, date_col, fare_col)
+    
+    g.fig.suptitle(f'Fare Trends over Flight Dates from {start_airport} Airport', y=1.02)
+    g.set_axis_labels('Flight Date', 'Total Fare')
+    
+    for ax in g.axes.flat:
+        for label in ax.get_xticklabels():
+            label.set_rotation(45)
+    
+    g.add_legend(title=hue_col)
+    
+    plt.tight_layout()
+    plt.show()
+    
+def plot_distance_histograms_by_destination(df, start_airport, dest_airport_col, dist_col, hue_col=None, bins=20):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    
+    df_filtered = df[df['startingAirport'] == start_airport]
+    
+    g = sns.FacetGrid(df_filtered, col=dest_airport_col, col_wrap=5, sharex=False, sharey=False, hue=hue_col)
+    
+    hist_kwargs = {'bins': bins, 'kde': False}
+    if hue_col:
+        hist_kwargs['hue'] = df_filtered[hue_col]
+    
+    g = g.map(sns.histplot, dist_col, **hist_kwargs)
+    
+    g.fig.suptitle(f'Histograms of Total Total Distance from {start_airport} Airport', y=1.02)
+    g.set_axis_labels('Total Distance ($)', 'Frequency')
+    
+    for ax in g.axes.flat:
+        for label in ax.get_xticklabels():
+            label.set_rotation(45)
+    
+    if hue_col:
+        g.add_legend()
+        
+    g.tight_layout()
+    plt.show()
+    
+def plot_day_of_week_fare(df, start_airport, dest_airport_col, fare_col, bins=20):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    df_filtered = df[df['startingAirport'] == start_airport]
+
+    g = sns.FacetGrid(df_filtered, col=dest_airport_col, col_wrap=5, sharex=False, sharey=False)
+
+    def create_barplot(data, **kwargs):
+        sns.barplot(data=data, x='day_of_week', y=fare_col, ax=plt.gca())
+
+    g.map_dataframe(create_barplot, data=df_filtered)
+
+    g.set_titles(col_template="{col_name} Airport")
+    g.set_axis_labels('Day of the Week', f'Mean {fare_col}')
+    
+    g.fig.suptitle(f'Day of the Week Mean Fare from {start_airport} Airport', y=1.02)
+
+    for ax in g.axes.flat:
+        for label in ax.get_xticklabels():
+            label.set_rotation(45)
+
+    g.tight_layout()
+    plt.show()
